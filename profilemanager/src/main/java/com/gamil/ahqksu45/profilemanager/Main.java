8b37bb3e540ae5c1828de27b3ad9f1ed829ebfb0
JangYunSung <ahsku45@naver,com>
2020-04-22 09:19:09 +0900

    2020-04-21 화요일

---
+++ profilemanager/src/main/java/com/gamil/ahqksu45/profilemanager/Main.java
@@ -0,0 +1,14 @@
+package com.gamil.ahqksu45.profilemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
