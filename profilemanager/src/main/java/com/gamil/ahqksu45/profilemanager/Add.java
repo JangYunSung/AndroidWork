277bf8af280d51c19324dc4144e954d3d763e091
JangYunSung <ahqksu45@gmail.com>
2020-04-23 11:50:54 +0900

    2020-04-21 목요일

---
+++ profilemanager/src/main/java/com/gamil/ahqksu45/profilemanager/Add.java
@@ -0,0 +1,17 @@
+package com.gamil.ahqksu45.profilemanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Add extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add);
+
+
+
+
+    }
+}
--- profilemanager/src/main/java/com/gamil/ahqksu45/profilemanager/Main.java
+++ profilemanager/src/main/java/com/gamil/ahqksu45/profilemanager/Main.java
@@ -2,13 +2,36 @@ package com.gamil.ahqksu45.profilemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Main extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+
+
+        ImageButton ibadd = findViewById(R.id.ibadd);
+        ibadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                Intent intent = new Intent(getApplicationContext(), Add.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
